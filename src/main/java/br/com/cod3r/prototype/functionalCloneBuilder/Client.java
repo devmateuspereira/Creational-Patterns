f77b123c06e13bf09003f448dbab2a0d66e191e1
Mateus Pereira Jorge <mateus.p.jorge@gmail.com>
2023-08-08 12:06:38 -0300

    final example of the design pattern prototype with builder

--- src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/Client.java
+++ src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/Client.java
@@ -5,8 +5,15 @@ import br.com.cod3r.prototype.functionalCloneBuilder.model.User;
 
 public class Client {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
-		User user = new User("ToClone", 25, new Address("ABC Street", 1000));
-		System.out.println(user);
-	}
+    public static void main(String[] args) throws CloneNotSupportedException {
+        User user = new User("ToClone", 25, new Address("ABC Street", 1000));
+        System.out.println(user);
+
+        User cloneUser = user.cloneBuilder().name("Cloned++").build();
+        System.out.println(cloneUser);
+
+        User anotherUser = cloneUser.cloneBuilder().age(100).address(new Address("A Street", 500)).build();
+        System.out.println(anotherUser);
+    }
+
 }
--- src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
+++ src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
@@ -1,31 +1,80 @@
 package br.com.cod3r.prototype.functionalCloneBuilder.model;
 
-public class User {
-	private String name;
-	private Integer age;
-	private Address address;
-	
-	public User(String name, Integer age, Address address) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.address = address;
-	}
-	
-	public String getName() {
-		return name;
-	}
+public class User implements Cloneable {
 
-	public Integer getAge() {
-		return age;
-	}
+    private String name;
 
-	public Address getAddress() {
-		return address;
-	}
+    private Integer age;
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
-	}
+    private Address address;
+
+    public User(String name, Integer age, Address address) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+    }
+
+    @Override
+    protected User clone() throws CloneNotSupportedException {
+        Address cloneAddress = (Address) address.clone();
+        return new User(name, age, cloneAddress);
+    }
+
+    public Builder cloneBuilder() {
+        return new Builder(name, age, address);
+    }
+
+    public static class Builder {
+
+        private String name;
+
+        private Integer age;
+
+        private Address address;
+
+        private Builder(String name, Integer age, Address address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder address(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, age, address);
+        }
+
+    }
+
 }
