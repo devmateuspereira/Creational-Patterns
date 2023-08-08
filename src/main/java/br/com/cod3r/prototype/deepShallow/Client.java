3baedc2671f9f3a4389dda0402a317feb3d9330d
Mateus Pereira Jorge <mateus.p.jorge@gmail.com>
2023-08-08 11:33:17 -0300

    second application of the design pattern prototype

--- src/main/java/br/com/cod3r/prototype/deepShallow/Client.java
+++ src/main/java/br/com/cod3r/prototype/deepShallow/Client.java
@@ -5,14 +5,14 @@ import br.com.cod3r.prototype.deepShallow.model.User;
 
 public class Client {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
-		User user = new User("Clone", 25, new Address("ABC Street", 1000));
-		System.out.println(user);
-		
-		User cloneUser = user.clone();
-		cloneUser.name = "Clone x2";
-		cloneUser.address.street = "Double Street";
-		System.out.println(user);
-		System.out.println(cloneUser);
-	}
+    public static void main(String[] args) throws CloneNotSupportedException {
+        User user = new User("Clone", 25, new Address("ABC Street", 1000));
+        System.out.println(user);
+        User cloneUser = user.clone();
+        cloneUser.name = "Clone x2";
+        cloneUser.address.street = "Double Street";
+        System.out.println(user);
+        System.out.println(cloneUser);
+    }
+
 }
